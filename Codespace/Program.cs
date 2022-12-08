91d03c3316b28595536f63e7c5bccba7757c490c
chand <chand@DESKTOP-OG4TMVM>
2022-12-07 22:08:49 -0800

    First commit -code space

---
+++ Codespace/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Codespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Sample
+    {
+        string Id { get; set; }
+        string Name { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
