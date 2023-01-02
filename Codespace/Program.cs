edde1dea22ac55b7508b5142bbec93a01b42f1c6
chandrasekarMariappan <chandrumca11@gmail.com>
2023-01-02 15:34:13 +0000

    new changes

--- Codespace/Program.cs
+++ Codespace/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+
 
 namespace Codespace
 {
@@ -11,12 +10,9 @@ namespace Codespace
              string connectionString = "ConnectionString";
 
             string input = Console.ReadLine();
-            using (var connection = new SqlConnection(connectionString))
+            
             {
                 var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" + input + "' ORDER BY PRICE";
-                var adapter = new SqlDataAdapter(query1, connection);
-                var result = new DataSet();
-                adapter.Fill(result);
                 Console.WriteLine("Completed");
                 Console.WriteLine("Completed");
             } 
